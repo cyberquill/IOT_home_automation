f40c0533cf2286c716d55155f55aad23f04b500b
Brijgopal <brijgopalbharadwaj1999@gmail.com>
2019-08-23 09:51:33 +0100

    Test2 successful - Firebase access achieved

--- fireTest2.py
+++ fireTest2.py
@@ -1,9 +1,29 @@
-from firebase import firebase
+import pyrebase
 
-firebase = firebase.FirebaseApplication('https://home-automation-iot-2019.firebaseio.com/', None)
-data = {'Name': 'John Doe',
-        'RollNo': 3,
-        'Percentage': 70.02
-        }
-result = firebase.post('/test', data)
-print(result)
+firebaseConfig = {
+    "apiKey": "AIzaSyAsNEthmi-tVhlaSH4fcNGi4Rr98Q7HSC8",
+    "authDomain": "home-automation-iot-2019.firebaseapp.com",
+    "databaseURL": "https://home-automation-iot-2019.firebaseio.com",
+    "projectId": "home-automation-iot-2019",
+    "storageBucket": "home-automation-iot-2019.appspot.com",
+    "messagingSenderId": "164364130987",
+    "appId": "1:164364130987:web:0596a80fbb65d7f9"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+# Get a reference to the database service
+db = firebase.database()
+
+# data to save
+data = {
+    "name": 'Adam'
+}
+
+#-----------------set new data-------------------
+results = db.child('user').set(data)
+#--------set new data or update existing---------
+results = db.child('user').set(data)
+#-----------------remove data--------------------
+results = db.child('user').remove()
+#------------------------------------------------
+print("done")
